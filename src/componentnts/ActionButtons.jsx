3fe7a675718b421e4e33e2203962c019e217b62a
vikash1622 <vy9983091105@gmail.com>
2025-06-21 12:30:11 +0530

    code

---
+++ src/componentnts/ActionButtons.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ActionButtons = () => {
+  return (
+        <div className="action-buttons">
+            <button className="print">Print</button>
+            <button className="close">Close</button>
+        </div>
+    );
+}
+
+export default ActionButtons
